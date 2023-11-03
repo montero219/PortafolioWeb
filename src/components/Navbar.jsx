3e0e589b4b52c8d71412a9d5db4343ed11f89eaa
Juan David Montero Lasso <juan.montero@movisoftware.co>
2023-11-03 09:34:10 -0500

    modificacion ruta archivo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import styles from './stylescomponent.css'
 const Navbar = () => {
   const descargarCV = () => {
-    const rutaCV = '/VD.pdf';
+    const rutaCV = 'VD.pdf';
     const enlace = document.createElement('a');
     enlace.href = rutaCV;
     enlace.download = 'cv_Juan_Montero.pdf';
