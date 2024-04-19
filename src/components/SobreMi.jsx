d8a71cb79cc10194443509aa11f64699f0f03946
Juan David Montero Lasso <juan.montero@movisoftware.co>
2024-04-19 12:10:52 -0500

    solucion problemas responsive

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -8,13 +8,13 @@ import me from '../assets/me.gif'
         <>
             <section className="containerSobremi">
                 <div className="containerDescriptionsobremi">
-                  <div style={{display: 'flex', height: '550px', border:'1px solid white' , padding:'20px', borderRadius:'16%'}}>
+                  <div  className="img-container-byme" style={{display: 'flex', height: '550px', border:'1px solid white' , padding:'20px', borderRadius:'16%'}}>
                   <img style={{borderRadius:'15%' , background:'rgb(255, 171, 3)'}}
                         src={me}
                         alt={"me from me"}
                         />
                   </div>
-                  <div style={{height: '473px',width: '610px',display:'grid'}}>
+                  <div className="container-text-byme" style={{height: '473px',width: '610px',display:'grid'}}>
                     <h1 style={{height:'20px'}}> ¿ Quien es Juan ?</h1>
                         <h2 style={{lineHeight:'1.5'}}>
                         Hola, <span style={{color:'#ffab03'}}>soy ingeniero electrónico</span> apasionado por el <span style={{color:'#ffab03'}}>desarrollo web.</span> Mi experiencia y habilidades técnicas son extensas y variadas, las cuales puedes explorar en detalle en la sección ‘Experiencia’. Sin embargo, creo firmemente que soy mucho más que mis logros profesionales.
